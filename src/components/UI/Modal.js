76394e23295383ba8c2d3e73ced55f1d5d808916
Jeff Ripke <jripke74@gmail.com>
2023-01-26 12:34:22 -0800

    add logic to manage cart and modal state

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
 
-const Backdrop = () => {
-  return <div className={classes.backdrop} />;
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,10 @@ const portalElemant = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElemant)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElemant
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElemant
