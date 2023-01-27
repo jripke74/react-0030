21ed3017db5e91ccb265acc4188ddce0886a9914
Jeff Ripke <jripke74@gmail.com>
2023-01-27 14:14:13 -0800

    add reducer() logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,10 +8,26 @@ const defaultCardState = {
 };
 
 const cartReducer = (state, action) => {
-  if (action.type = 'ADD') {
-    const updatedItems = state.items.concat(action.item);
+  if ((action.type = 'ADD')) {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
